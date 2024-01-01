b83704d6e03bd36fc13a5b90bfbd9db97ed8f64c
Ceaseless04 <kristian120304@gmail.com>
2024-01-01 13:03:22 -0500

    finished file metadata freecodecamp project

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
-
 var app = express();
+const multer_file = require('multer');
+require('dotenv').config()
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +11,20 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// POST request for file upload
+app.post('/api/fileanalyse', multer_file({}).single('upfile'), (req, res) => {
+  const name = req.file.originalname;
+  const type = req.file.mimetype;
+  const size = req.file.size;
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+  res.json({ name, type, size });
+});
 
+app.get('/api/fileanalyse',)
 
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
